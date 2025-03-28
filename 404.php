d7cdc311511488014fc30927fd1792d2e026051b
shohei1227 <shohei_n2826@icloud.com>
2025-03-29 00:28:07 +0900

    404からget_footerを削除

--- 404.php
+++ 404.php
@@ -20,5 +20,20 @@ get_header();
             </a>
         </div>
     </div>
-    <?php get_footer(); ?>
+    <footer id="footer">
+            <div class="foot-wrapper">
+                <div class="flex">
+                    <div class="logo">
+                        <img src="">
+                    </div>
+                    <div class="info">
+                        <p>
+                            早稲田大学 創造理工学部 経営システム工学科（西早稲田キャンパス）<br>
+                            〒169-8555 東京都新宿区大久保3-4-1 51号館15階
+                        </p>
+                    </div>
+                </div>
+                <p class="copyright">Copyright &copy; Waseda University, Ohmori Lab, 2024</p>
+            </div>
+        </footer>
 </body>
