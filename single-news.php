bab711b67eea7d9d695186e4d032339dee64f6e4
shohei1227 <shohei_n2826@icloud.com>
2025-03-29 00:47:44 +0900

    newsページのfotterの読み込み変更

--- single-news.php
+++ single-news.php
@@ -13,4 +13,20 @@
     <div class="ohmorilab__page flex-grow items-center justify-center mx-auto my-10 max-w-[1000px] w-4/5">
         <?php the_content(); ?>
     </div>
+    <footer id="footer">
+        <div class="foot-wrapper">
+            <div class="flex">
+                <div class="logo">
+                    <img src="">
+                </div>
+                <div class="info">
+                    <p>
+                        早稲田大学 創造理工学部 経営システム工学科（西早稲田キャンパス）<br>
+                        〒169-8555 東京都新宿区大久保3-4-1 51号館15階
+                    </p>
+                </div>
+            </div>
+            <p class="copyright">Copyright &copy; Waseda University, Ohmori Lab, 2024</p>
+        </div>
+    </footer>
 </div>
